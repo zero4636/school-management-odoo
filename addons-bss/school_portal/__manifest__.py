b61abd59994d843307de7db1632110795d8da8e9
zero4636 <tadinhdong4636@gmail.com>
2025-10-02 11:19:23 +0000

    Update view school_portal

--- addons-bss/school_portal/__manifest__.py
+++ addons-bss/school_portal/__manifest__.py
@@ -2,27 +2,15 @@
     "name": "School Portal",
     "summary": "Portal for Students and Teachers",
     "version": "1.0",
-    "depends": ["base", "portal", "website", "school_core"],
+    "depends": ["portal", "website", "school_core"],
     "author": "Dong TD",
     "website": "https://odoo.test",
     "category": "Education",
     "data": [
-        "views/portal_templates.xml",
         "views/portal_student.xml",
-        "views/portal_teacher.xml",
-        "views/legacy_widget_templates.xml",
-        "views/owl_templates.xml",
+        "views/portal_teacher.xml"
     ],
-    "assets": {
-        "web.assets_frontend": [
-            "school_portal/static/src/js/student_status.js",
-            "school_portal/static/src/scss/portal.scss",
-        ],
-        "web.assets_qweb": [
-            "school_portal/static/src/xml/*.xml",
-        ],
-    },
     "installable": True,
     "application": True,
-    "auto_install": False,
+    "auto_install": True
 }
--- addons-bss/school_portal/controllers/portal.py
+++ addons-bss/school_portal/controllers/portal.py
@@ -3,12 +3,12 @@ from odoo.http import request
 
 class SchoolPortal(http.Controller):
 
-    @http.route(['/my/student'], type='http', auth="user", website=True)
+    @http.route(['/my/student'], type='http', auth='public', website=True)
     def portal_student(self, **kw):
-        student = request.env['school.student'].sudo().search([('user_id', '=', request.env.user.id)], limit=1)
-        return request.render("school_portal.portal_student_page", {"student": student})
+        student = request.env['school.student'].sudo().search([])
+        return request.render("school_portal.portal_student_page", {"students": student})
 
-    @http.route(['/my/teacher'], type='http', auth="user", website=True)
+    @http.route(['/my/teacher'], type='http', auth='public', website=True)
     def portal_teacher(self, **kw):
-        teacher = request.env['school.teacher'].sudo().search([('user_id', '=', request.env.user.id)], limit=1)
-        return request.render("school_portal.portal_teacher_page", {"teacher": teacher})
+        teacher = request.env['school.teacher'].sudo().search([])
+        return request.render("school_portal.portal_teacher_page", {"teachers": teacher})
